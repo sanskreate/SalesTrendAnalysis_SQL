a57d0e40541967bff02b55180d3192e4a0afa226
sanskreate <123264981+sanskreate@users.noreply.github.com>
2025-04-15 16:04:00 +0530

    Create OnlineSalesdb.sql

---
+++ DB_CREATION/OnlineSalesdb.sql
@@ -0,0 +1,14 @@
+-- Database: OnlineSales
+
+-- DROP DATABASE IF EXISTS "OnlineSales";
+
+CREATE DATABASE "OnlineSales"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en-US'
+    LC_CTYPE = 'en-US'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
